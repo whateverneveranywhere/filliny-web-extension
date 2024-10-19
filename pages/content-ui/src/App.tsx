aeb7ddbb7f43a219c46b37c4718f7c6b0c2fa035
A.B <153488541+ava-shakewell@users.noreply.github.com>
2024-10-19 11:18:41 +0200

    feat(disable): here should be the code for popup instead loading conditionally

--- pages/content-ui/src/App.tsx
+++ pages/content-ui/src/App.tsx
@@ -1,23 +1,24 @@
 import { useEffect } from 'react';
-import { TailwindButton } from '@extension/ui';
-import { useStorage } from '@extension/shared';
-import { exampleThemeStorage } from '@extension/storage';
+// import { TailwindButton } from '@extension/ui';
+// import { useStorage } from '@extension/shared';
+// import { exampleThemeStorage } from '@extension/storage';
 
 export default function App() {
-  const theme = useStorage(exampleThemeStorage);
+  // const theme = useStorage(exampleThemeStorage);
 
   useEffect(() => {
     console.log('content ui loaded');
   }, []);
 
   return (
-    <div className="flex items-center justify-between gap-2 rounded bg-blue-100 px-2 py-1">
-      <div className="flex gap-1 text-blue-500">
-        Edit <strong className="text-blue-700">pages/content-ui/src/app.tsx</strong> and save to reload.
-      </div>
-      <TailwindButton theme={theme} onClick={exampleThemeStorage.toggle}>
-        Toggle Theme
-      </TailwindButton>
-    </div>
+    // <div className="flex items-center justify-between gap-2 rounded bg-blue-100 px-2 py-1">
+    //   <div className="flex gap-1 text-blue-500">
+    //     Edit <strong className="text-blue-700">pages/content-ui/src/app.tsx</strong> and save to reload.
+    //   </div>
+    //   <TailwindButton theme={theme} onClick={exampleThemeStorage.toggle}>
+    //     Toggle Theme
+    //   </TailwindButton>
+    // </div>
+    <></>
   );
 }
