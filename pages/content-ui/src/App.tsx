c773e3b68c1d429159e35b97162006a82ce2fad8
A.B <153488541+ava-shakewell@users.noreply.github.com>
2024-11-05 14:46:17 +0100

    fix(css): global var name changes

--- pages/content-ui/src/App.tsx
+++ pages/content-ui/src/App.tsx
@@ -3,21 +3,6 @@ import { profileStrorage } from '@extension/storage';
 import { FillinyButton } from '@extension/ui';
 import { useEffect, useMemo, useState } from 'react';
 
-// export default function App() {
-//   useEffect(() => {
-//     console.log('content ui loaded');
-//   }, []);
-
-//   return (
-//     <div className="flex items-center justify-between gap-2 rounded bg-blue-100 px-2 py-1">
-//       <div className="flex gap-1 text-blue-500">
-//         Edit <strong className="text-blue-700">pages/content-ui/src/app.tsx</strong> and save to reload.
-//       </div>
-//       <Button onClick={exampleThemeStorage.toggle}>Toggle Theme</Button>
-//     </div>
-//   );
-// }
-
 export default function App() {
   const defaultProfile = useStorage(profileStrorage);
   const [currentVisitingWebsiteUrl, setCurrentVisitingWebsiteUrl] = useState('');
