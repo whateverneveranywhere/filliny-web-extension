c637f5e3c904a342b34e4156a0fe6b71e88e980f
A.B <153488541+ava-shakewell@users.noreply.github.com>
2025-01-25 09:44:36 +0100

    refactor: simplify manifest configuration and remove unused sections
    - Remove commented-out chrome_url_overrides sections
    - Remove options_page and devtools_page configurations
    - Improve extension name display with environment-specific formatting
    - Streamline manifest structure for cleaner configuration

--- chrome-extension/manifest.js
+++ chrome-extension/manifest.js
@@ -40,7 +40,7 @@ const manifest = withSidePanel({
    * if you want to support multiple languages, you can use the following reference
    * https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Internationalization
    */
-  name: '__MSG_extensionName__' + ' | ' + env,
+  name: `__MSG_extensionName__${env !== 'prod' ? ` (${env.toUpperCase()})` : ''}`,
   version: packageJson.version,
   description: '__MSG_extensionDescription__',
   host_permissions: [
@@ -50,14 +50,10 @@ const manifest = withSidePanel({
     ...(env === 'prod' ? ['https://app.filliny.io/*'] : []),
   ],
   permissions: ['storage', 'scripting', 'tabs', 'cookies', 'activeTab', 'notifications'],
-  options_page: 'options/index.html',
   background: {
     service_worker: 'background.iife.js',
     type: 'module',
   },
-  // chrome_url_overrides: {
-  //   newtab: 'new-tab/index.html',
-  // },
   icons: {
     128: 'icon-128.png',
   },
@@ -75,7 +71,6 @@ const manifest = withSidePanel({
       css: ['content.css'], // public folder
     },
   ],
-  devtools_page: 'devtools/index.html',
   web_accessible_resources: [
     {
       resources: ['*.js', '*.css', '*.svg', 'icon-128.png', 'icon-34.png'],
@@ -86,9 +81,6 @@ const manifest = withSidePanel({
     default_popup: 'popup/index.html',
     default_icon: 'icon-34.png',
   },
-  // chrome_url_overrides: {
-  //   newtab: 'new-tab/index.html',
-  // },
 });
 
 export default manifest;
